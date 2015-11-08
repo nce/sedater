6cae7bbe3ee02d925c21270d7f6b928573465619
Ulli Goschler <ulligoschler@gmail.com>
2015-11-08 23:12:25 +0100

    typo

---
+++ sedater/test/test_exporter.py
@@ -0,0 +1,12 @@
+# ./sedater/test/test_exporter.py
+# Author:   Ulli Goschler <ulligoschler@gmail.com>
+# Created:  Sun, 08.11.2015 - 23:09:28 
+# Modified: Sun, 08.11.2015 - 23:11:53
+
+import unittest
+
+from sedater.export import CSVExporter
+
+class TestCSVExporter(unittest.TestCase):
+    # TODO: implement
+    pass
