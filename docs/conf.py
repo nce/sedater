b98d6b90e3bce12ba1235f637acbde0ceff2005c
Ulli Goschler <ulligoschler@gmail.com>
2015-11-08 18:51:43 +0100

    rtd setup...

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,16 @@
 import sys
 import os
 import shlex
+import unittest.mock as mock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
+MOCK_MODULES = ['typing']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
