c30b546f77c5f8abd4c2de57ca7be7d97d39ebec
Ulli Goschler <ulligoschler@gmail.com>
2015-11-08 18:35:39 +0100

    rtd setup...

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+        name='Sedater',
+        version='0.0.1',
+        description='Python Tool to convert SensorValidation Data to '
+        'a more machine-processable format',
+        author='Ulli Goschler',
+        author_email='ulligoschler@gmail.com',
+        license = "MIT",
+        url='https://github.com/nce/sedater',
+        packages=['sedater', 'sedater.lib', 'tests'],
+        long_description=read('README.md'),
+     )
