9f0ef5a01586a39a358aa2034e818ebd8448b118
Ulli Goschler <ulligoschler@gmail.com>
2015-12-08 23:04:59 +0100

    tests for exporter added

--- sedater/test/test_exporter.py
+++ sedater/test/test_exporter.py
@@ -1,7 +1,7 @@
 # ./sedater/test/test_exporter.py
 # Author:   Ulli Goschler <ulligoschler@gmail.com>
 # Created:  Sun, 08.11.2015 - 23:09:28 
-# Modified: Mon, 09.11.2015 - 12:11:25
+# Modified: Tue, 08.12.2015 - 23:04:08
 
 import unittest
 import os
@@ -10,7 +10,51 @@ from testfixtures import TempDirectory
 import sys
 
 from sedater.export import CSVExporter
-from sedater.lib import shared
+
+from sedater.export import XMLExporter
+from xml.etree import ElementTree as et
+
+from sedater.lib import shared as lib
+
+class TestXMLExporter(unittest.TestCase):
+    def setUp(self):
+        self.tmp = TempDirectory()
+        self.dir = self.tmp.makedir('foobar')
+        self.exp = XMLExporter(self.dir)
+    def tearDown(self):
+        TempDirectory.cleanup_all()
+
+    def test_creation_of_meta_info_file(self):
+        filename = 'barfoo.xml'
+        status = self.exp._exportMetaInformationToXML('', '', filename)
+        self.assertTrue(os.path.isfile(self.dir+'metainformation.xml'))
+        self.assertTrue(status)
+    def test_content_of_meta_info_file(self):
+        filename = 'barfoo.xml'
+        status = self.exp._exportMetaInformationToXML('01', '02', filename)
+        tree = et.parse(self.dir+'metainformation.xml')
+        xml = tree.getroot()
+        session = xml.find('Session')
+        exercise = xml.find('Exercise')
+        annotation = xml.find('Annotationfile')
+        self.assertEquals('01', session.text)
+        self.assertEquals('02', exercise.text)
+        self.assertEquals('barfoo.xml', annotation.text)
+    def test_content_of_xml_export_file(self):
+        src = lib.Sourcefile._make(['', '', '', '', lib.Orientation.left])
+        input = lib.Validationfile._make([src, 'Gold', [['Foo', 'Bar']], 
+            [{ 'Fu': '1', 'Baz': '2'}, {'Fu': '3', 'Baz': '4'}]])
+        self.assertTrue(self.exp.export(input))
+        tree = et.parse(self.dir + 'annotation.xml')
+        xml = tree.getroot()
+        validation = xml.find('Validation')
+        self.assertEquals(validation.attrib, {'Type': 'Gold'})
+        sensor = validation.find('Sensor')
+        self.assertEquals(sensor.attrib, {'Orientation': 'left'})
+
+        # with open(self.dir + 'annotation.xml') as f:
+        #     print(f.read(), file=sys.stderr)
+        # print(sensor.attrib,file=sys.stderr)
 
 class TestCSVExporter(unittest.TestCase):
     def setUp(self):
@@ -21,14 +65,14 @@ class TestCSVExporter(unittest.TestCase):
         TempDirectory.cleanup_all()
     def test_creation_of_export_file(self):
         filename = 'foobar.csv'
-        values = [shared.Sensorsegment._make([0] * 6)]
+        values = [lib.Sensorsegment._make([0] * 6)]
         status = self.exp.export(values, self.dir+filename)
         self.assertTrue(os.path.isfile(self.dir+filename))
         self.assertTrue(status)
     def test_content_of_export_file(self):
         filename = 'foobar.csv'
-        values = [shared.Sensorsegment._make([0] * 6),
-                shared.Sensorsegment._make([1] * 6)
+        values = [lib.Sensorsegment._make([0] * 6),
+                lib.Sensorsegment._make([1] * 6)
                 ]
         self.exp.export(values, self.dir+filename, False, False)
         res = self.tmp.read(self.dir+filename)
@@ -36,21 +80,21 @@ class TestCSVExporter(unittest.TestCase):
         self.assertEqual(ref, res)
     def test_content_with_header_with_indices(self):
         filename = 'foobar.csv'
-        values = [shared.Sensorsegment._make([0] * 6)]
+        values = [lib.Sensorsegment._make([0] * 6)]
         self.exp.export(values, self.dir+filename, True, True)
         res = self.tmp.read(self.dir+filename)
         ref = b'Index,accelX,accelY,accelZ,gyroX,gyroY,gyroZ\r\n1,0,0,0,0,0,0\r\n'
         self.assertEqual(ref, res)
     def test_content_with_header_without_indices(self):
         filename = 'foobar.csv'
-        values = [shared.Sensorsegment._make([0] * 6)]
+        values = [lib.Sensorsegment._make([0] * 6)]
         self.exp.export(values, self.dir+filename, True, False)
         res = self.tmp.read(self.dir+filename)
         ref = b'accelX,accelY,accelZ,gyroX,gyroY,gyroZ\r\n0,0,0,0,0,0\r\n'
         self.assertEqual(ref, res)
     def test_content_without_header_with_indices(self):
         filename = 'foobar.csv'
-        values = [shared.Sensorsegment._make([0] * 6)]
+        values = [lib.Sensorsegment._make([0] * 6)]
         self.exp.export(values, self.dir+filename, False, True)
         res = self.tmp.read(self.dir+filename)
         ref = b'1,0,0,0,0,0,0\r\n'
