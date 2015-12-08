7304910902e09f46ca9bac16cf5253bee915bcac
Ulli Goschler <ulligoschler@gmail.com>
2015-12-08 23:04:31 +0100

    xml exporter added

--- sedater/export.py
+++ sedater/export.py
@@ -1,9 +1,11 @@
 # ./sedater/export.py
 # Author:   Ulli Goschler <ulligoschler@gmail.com>
 # Created:  Sun, 08.11.2015 - 22:21:45 
-# Modified: Sat, 05.12.2015 - 18:05:42
+# Modified: Tue, 08.12.2015 - 22:41:41
 
 import csv
+import os
+from xml.etree import ElementTree as et
 
 import sys
 
@@ -20,53 +22,132 @@ class Exporter(object):
     def __init__(self):
         pass
 
+# # TODO: move to validation; this should not be the in an exporter
+#     def getOrientation(self, validationProperties):
+#         orientationIndex = validationProperties.index('Orientation')
+#         # get the next element in list
+#         orientation = validationProperties[orientationIndex + 1]
+#         # check if orientation is a sring, and is a valid 'Orientation'
+#         if isinstance(orientation, str) and \
+#                 orientation in [e.name for e in shared.Orientation]:
+#             return orientation
+#         else:
+#             # TODO: get current file
+#             raise AttributeError("The ValidationTxtFile '{}' does not contain"
+#                     " a valid Orientation. This is not recoverable, as we"
+#                     " don't know where to store the export".format("Unknown"))
+
+#     def getValidationType(self, validationProperties):
+#         validationIndex = validationProperties.index('Validation')
+#         # get element 'right of' Validation
+#         validation = validationProperties[validationIndex + 1]
+#         if isinstance(validation, str):
+#             return validation
+#         else:
+#             raise AttributeError("The ValidationTxtFile '{}' does not contain"
+#                     " a valid ValidationType. This is recoverable, but might"
+#                     " mishandle the export".format("Unknown"))
+
 class XMLExporter(Exporter):
-    def __init__(self, session, exercise, exportLocation):
+    def __init__(self, exportLocation):
         """
         Exports the given validation to a specific file in XML notation
 
-        :param str session: The releated session
-        :param str exercise: The related exercise
         :param str exportLocation: Path to the export directory
         """
 
-        self.session  = session
-        self.exercise = exercise
         self.exportLocation = exportLocation
         self.annotationFile = 'annotation.xml'
-    def export(self, validationdata):
+    def export(self, validation):
         """
-        :param validationdata: The parsed :class:`Textfile <sedater.txtvalidation.TxtConverter.parseTxtFile>`
-        :param type validationdata: tuple of ``list`` of ``list`` and ``list`` of ``dictionary``
+        :param validation: The parsed 
+            :class:`Textfile <sedater.txtvalidation.TxtConverter.parseTxtFile>`
+        :param type validation: 
+            :class:`Validationfile <sedater.lib.shared.Validationfile>`
         """
 
         filename = 'annotation.xml'
-        self._exportMetaInformationToXml(filename)
+        # extract parameters from txtvalidation
 
-    def _exportMetaInformationToXML(self, annotationFile):
+        self._exportMetaInformationToXML(validation.sourcefile.session, 
+                validation.sourcefile.exercise, filename)
+
+        # check if file already exists (from a previous run or the other sensors)
+        if os.path.isfile(self.exportLocation + filename):
+            document = et.ElementTree()
+            document.parse(self.exportLocation + filename)
+
+            overwrite = document.find('.//Sensor [@Orientation="'+
+                    validation.sourcefile.orientation.name +'"]/..\
+                    [@Type="'+validation.type+'"]')
+            if overwrite:
+                document.getroot().remove(overwrite)
+        else:
+            head = et.Element('Data')
+            document = et.ElementTree(head)
+
+        document = document.getroot()
+        if validation.type:
+            validationXML = et.SubElement(document, 'Validation')
+            validationXML.set('Type', validation.type)
+        else:
+            validationXML = document
+
+        orientationXML = et.SubElement(validationXML, 'Sensor')
+        orientationXML.set('Orientation', validation.sourcefile.orientation.name)
+
+        meta = et.SubElement(orientationXML, 'Meta')
+        #print(validation.properties, file=sys.stderr)
+        for prop in validation.properties:
+            tmp = et.SubElement(meta, prop[0])
+            tmp.text = prop[1]
+
+        content = et.SubElement(orientationXML, 'Content')
+        steps = 1
+        for prop in validation.content:
+            counter = et.SubElement(content, 'No' + str(steps))
+            for key, value in prop.items():
+                tmp = et.SubElement(counter, key)
+                tmp.text = value
+            steps = steps + 1
+
+        export = et.ElementTree(document)
+        shared.indentXML(document)
+        export.write(self.exportLocation + filename, encoding='utf-8',
+                xml_declaration=True)
+        return True
+
+
+
+    def _exportMetaInformationToXML(self, session, exercise, annotationFile):
         """
-        Exports the metainformation to a seperate XML file
+        Creates a XML file containing the session details. Specifying the 
+        ``Session``, ``Exercise`` and the fll path to the ``Annotationfile`` 
+        containing all Validationdata.
 
+        :param str session: Name of the current session
+        :param str exercise: Name of the current exercise
         :param str annotationFile: Full path to the annotation file
         """
         filename = 'metainformation.xml'
 
         # Build XML structure
-        document = et.Element('meta')
+        documentXML = et.Element('Meta')
 
-        session = et.SubElement(document, 'Session')
-        session.text = self.session
+        sessionXML = et.SubElement(documentXML, 'Session')
+        sessionXML.text = session
 
-        exercise = et.SubElement(document, 'Exercise')
-        exercise.text = self.exercise
+        exerciseXML = et.SubElement(documentXML, 'Exercise')
+        exerciseXML.text = exercise
 
-        annotation = et.SubElement(document, 'Annotationfile')
-        annotation.text = annotationFile
+        annotationXML = et.SubElement(documentXML, 'Annotationfile')
+        annotationXML.text = annotationFile
 
-        export = et.ElementTree(document)
-        shared.indentXML(document)
-        export.write(self.exportLocation + filename, encoding='utf-8', 
+        exportXML = et.ElementTree(documentXML)
+        shared.indentXML(documentXML)
+        exportXML.write(self.exportLocation + filename, encoding='utf-8', 
                 xml_declaration=True)
+        return True
 
 
 class CSVExporter(Exporter):
