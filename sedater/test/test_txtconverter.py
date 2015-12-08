0f19749cfb62b86640fe295601cc03b62cbcd790
Ulli Goschler <ulligoschler@gmail.com>
2015-12-08 23:06:09 +0100

    handle new Validationobject

--- sedater/test/test_txtconverter.py
+++ sedater/test/test_txtconverter.py
@@ -1,13 +1,13 @@
 # ./sedater/test/test_txtconverter.py
 # Author:   Ulli Goschler <ulligoschler@gmail.com>
 # Created:  Tue, 10.11.2015 - 20:04:24 
-# Modified: Tue, 10.11.2015 - 23:44:24
+# Modified: Tue, 08.12.2015 - 21:59:36
 
 import unittest
 from testfixtures import TempDirectory
 
 from sedater.txtvalidation import TxtConverter
-from sedater.lib import shared
+from sedater.lib import shared as lib
 
 import sys
 
@@ -16,16 +16,18 @@ class TestTxtConverter(unittest.TestCase):
         self.tmp = TempDirectory()
         self.ref = self.tmp.write('txtvalidation.txt', 
                 b"Foo, Bar\nFaz, Baz\n1,2\n3,4")
-        self.reffile = shared.Sourcefile._make([self.ref, 'txtvalidation.txt', '', '', ''])
+        self.reffile = lib.Sourcefile._make([self.ref, 'txtvalidation.txt', '', '', ''])
     def tearDown(self):
         TempDirectory.cleanup_all()
     def test_correct_txt_file(self):
-        reffile = self.tmp.write('txtvalidation.txt', 
+        inFile = self.tmp.write('txtvalidation.txt', 
                 b"Foo, Bar\nFaz, Baz\n1,2\n3,4")
-        refsource = shared.Sourcefile._make([reffile, 
+        inSource = lib.Sourcefile._make([inFile, 
             'txtvalidation.txt', '', '', ''])
-        ref = ([['Foo', ' Bar']], [{' Baz': '2', 'Faz': '1'}, 
-            {' Baz': '4', 'Faz': '3'}])
+        # ref = ([['Foo', ' Bar']], [{' Baz': '2', 'Faz': '1'}, 
+        #     {' Baz': '4', 'Faz': '3'}])
+        ref = lib.Validationfile._make([inSource, '', [['Foo', 'Bar']],
+            [{'Baz': '2', 'Faz': '1'}, {'Baz': '4', 'Faz': '3'}]])
         conv = TxtConverter([])
-        res = conv.parseTxtFile(refsource)
+        res = conv.parseTxtFile(inSource)
         self.assertEqual(ref,res)
